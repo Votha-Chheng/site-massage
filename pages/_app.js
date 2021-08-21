5c035ec6794ca829c06094db6990600fb6a3b05f
Dan-Electro <74511348+Votha-Chheng@users.noreply.github.com>
2021-08-21 14:38:33 +0200

    version 1

--- pages/_app.js
+++ pages/_app.js
@@ -10,8 +10,10 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1"/>
         <meta name="description" content="Baux'T des sens massage à domicile et yoga Arles Alpilles & Baux de Provence"/>
         <meta property="og:title" content="Baux'T des sens, massage à domicile et yoga Arles Alpilles & Baux de Provence." />
-        <meta property="og:image" content="https://zupimages.net/up/21/33/4t0a.jpg" type="jpg/jpeg" />
+        <meta property="og:image" content="https://zupimages.net/up/21/33/4t0a.jpg"/>
+        <meta property="og:image:type" content="image/jpg"/>
         <meta property="og:type" content="website" />
+        <meta property="og:locale" content="fr" />
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet"/>
